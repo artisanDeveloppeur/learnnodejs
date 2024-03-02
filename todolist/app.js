99fbaa26d1abcc633c41a4fed9aacf660efd2ac8
Denis <DenisArtisanDeveloppeur@gmail.com>
2024-03-02 13:33:00 +0100

    liste fichier octet

--- todolist/app.js
+++ todolist/app.js
@@ -6,30 +6,33 @@ import { readdir, stat } from "node:fs/promises"
 const wait = (duration) => new Promise(resolve => setTimeout(resolve, duration))
 console.time('code')
 const files = await readdir('./', { withFileTypes: true })
-for (const file of files) {
-  const parts = [
-    file.isDirectory() ? 'D' : 'F',
-    file.name
-  ]
-  if (!file.isDirectory()) {
-    //const { size } = await stat(file.name)
-    const size = await wait(1000)
-    parts.push(`${size}octet`)
-  }
-  // destructuration
-  //const { size } = await stat(file.name)
-  //console.log(`${file.isDirectory() ? 'D' : 'F'} - ${file.name} - ${size}octet`)
-  console.log(parts.join(' - '))
-}
-//console.log(files)
+await Promise.allSettled(
+  files.map(async (file) => {
+    const parts = [
+      file.isDirectory() ? 'D' : 'F',
+      file.name
+    ]
+    if (!file.isDirectory()) {
+      const { size } = await stat(file.name)
+      //const size = await wait(1000)
+      parts.push(`${size}octet`)
+    }
+
+    console.log(parts.join(' - '))
+  })
+)
+//for (const file of files) {
+
+
+//}
 console.timeEnd('code')
 
+/**
+import { readFile } from 'node:fs/promises'
+import { fileURLToPath } from 'node:url'
+import { dirname, join } from 'node:path'
 
-//import { watch } from "fs/promises"
-
-/*
-const watcher = await watch('./')
-for await (const event of watcher) {
-  console.log(event)
-}
-*/
+const dir = dirname(fileURLToPath(import.meta.url))
+const filename = join(dir, 'fichier.txt')
+console.log(await readFile(filename, { encoding: 'utf8' }))
+ */
