c01a05717836e331be04a02d95d2774b3d037f0b
Denis <DenisArtisanDeveloppeur@gmail.com>
2024-03-02 09:55:26 +0100

    dirname and join

--- todolist/app.js
+++ todolist/app.js
@@ -1,17 +1,4 @@
-//import fs from 'node:fs'
-//import { readFile } from 'node:fs/promises'
-import { stat, writeFile } from "fs/promises"
-const info = await stat('demo.txt')
-console.log(info)
-//const content = await readFile('demo.txt', { encoding: 'utf8' })
-//console.log(content)
-
-//const content = fs.readFileSync('demo.txt', { encoding: 'utf8' })
-//console.log(content)
-/*
-const content = fs.readFile('demo.txt', { encoding: 'utf8' }, function (err, content) {
-  console.log(content)
-
-})
-*/
-//console.log('Learn nodejs')
+import { open } from "fs/promises"
+const file = await open('demo.txt', 'a')
+file.write('ecrit à la fin')
+file.close
