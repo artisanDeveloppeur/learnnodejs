7d9197f7aa6bc008e0b9c24735935c34166b72e7
Denis <DenisArtisanDeveloppeur@gmail.com>
2024-03-04 14:01:15 +0100

    with param

--- todolist/app.js
+++ todolist/app.js
@@ -4,8 +4,11 @@ import { createServer } from 'node:http'
 const server = createServer((req, res) => {
   //console.log(req.url)
   const url = new URL(req.url, `http://${req.headers.host}`)
-  console.log(url)
+  res.write(`Bonjour ${url.searchParams.get('name')}`)
+  res.end()
+  //console.log(url)
   //console.log(req.headers.accept)
+  /*
   const file = createReadStream('index.html')
   res.writeHead(404, {
     'Content-Type': 'text/html'
@@ -13,10 +16,11 @@ const server = createServer((req, res) => {
   file.pipe(res, { end: false })
   file.on('end', () => {
     res.end()
-  })
-  //res.write('hello server')
-  //res.end()
+  */
 })
+
 server.listen('8888')
 
 // http://localhost:8888/
+
+// http://localhost:8888/?name=john
