7e3eb7a23b402eb0aacb280c7c360dea8a24f3a6
Denis <DenisArtisanDeveloppeur@gmail.com>
2024-03-04 14:54:12 +0100

    json insomnia

--- todolist/app.js
+++ todolist/app.js
@@ -5,10 +5,10 @@ import { json, text } from 'node:stream/consumers';
 const server = createServer(async (req, res) => {
   //console.log(req.url)
   const url = new URL(req.url, `http://${req.headers.host}`);
-  let body = ''
-  console.log(await text(req))
+  //let body = ''
+  //console.log(await text(req))
   // Use await inside an async function
-  //res.write(`Bonjour ${(await json(req)).name}`);
+  res.write(`Bonjour ${(await json(req)).name}`);
   res.end();
 });
 server.listen('8888')
