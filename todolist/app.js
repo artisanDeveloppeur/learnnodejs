0a52b48c6acd604881cf8decbb4a883faa51b1d3
Denis <DenisArtisanDeveloppeur@gmail.com>
2024-03-06 12:11:00 +0100

    child process 10 min

---
+++ todolist/app.js
@@ -0,0 +1,1 @@
+console.log('node child process change')
--- todolist/watcher.js
+++ todolist/watcher.js
@@ -1,15 +1,32 @@
 import { exec, spawn } from 'node:child_process'
+import { watch } from 'node:fs/promises'
 const [node, _, file] = process.argv
-const pr = spawn('dir', [], { shell: true })
-pr.stdout.on('data', (data) => {
-  console.log(data.toString('utf8'))
-})
-pr.stderr.on('data', (data) => {
-  console.error(data.toString('utf8'))
-})
-pr.once('close', (code) => {
-  console.log(`Process exited :${code}`)
-})
+
+function spawnNode() {
+  const pr = spawn(node, [file])
+  pr.stdout.on('data', (data) => {
+    console.log(data.toString('utf8'))
+  })
+  pr.stderr.on('data', (data) => {
+    console.error(data.toString('utf8'))
+  })
+  pr.once('close', (code) => {
+    console.log(`Process exited :${code}`)
+  })
+
+  return pr
+}
+
+let childNodeProcess = spawnNode()
+const watcher = watch('./', { recursive: true })
+for await (const event of watcher) {
+  if (event.filename.endsWith('js')) {
+    childNodeProcess.kill()
+    childNodeProcess = spawnNode()
+    console.log(event)
+
+  }
+}
 
 /*
 ou stdout
