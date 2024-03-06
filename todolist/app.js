a274122d1d802e3b1c2cbc857e64f7ed3280ef66
Denis <DenisArtisanDeveloppeur@gmail.com>
2024-03-06 15:55:27 +0100

    404

--- todolist/app.js
+++ todolist/app.js
@@ -3,8 +3,31 @@ import { findTodos } from './functions/todos_storage.js';
 
 const server = createServer(async (req, res) => {
   res.setHeader('Content-Type', 'application/json')
-  const todos = await findTodos()
-  res.write(JSON.stringify(todos))
+
+  const url = new URL(req.url, `http://${req.headers.host}`)
+  if (url.pathname === '/todos') {
+    const todos = await findTodos()
+    res.write(JSON.stringify(todos))
+  } else {
+    res.writeHead(404)
+  }
+  console.log(url)
+  /**
+    URL {
+    href: 'http://localhost:8888/todos',
+    origin: 'http://localhost:8888',
+    protocol: 'http:',
+    username: '',
+    password: '',
+    host: 'localhost:8888',
+    hostname: 'localhost',
+    port: '8888',
+    pathname: '/todos',
+    search: '',
+    searchParams: URLSearchParams {},
+    hash: ''
+  }
+   */
   res.end();
 });
 server.listen('8888')
